a7e5c47e1288d179636dc431947cc26ba8e10948
José Rodrigues <jose.rodrigues.emailpessoal@gmail.com>
2018-10-02 12:39:18 +0000

    Create CLI app

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# page-bone-cli
-A CLI app to create new project usign page-bone Boilerplate
+# pagebone-cli
+
+> :computer: A simple CLI app to create new project using pagebone Boilerplate
+
+[![npm](https://img.shields.io/npm/v/pagebone-cli.svg?colorB=green)](https://www.npmjs.com/package/pagebone-cli)
+[![NpmLicense](https://img.shields.io/npm/l/pagebone-cli.svg?colorB=brightgreen)](https://www.npmjs.com/package/pagebone-cli)
+
+
+## How it works?
+
+![pagebone](https://user-images.githubusercontent.com/6127099/46318395-b49c7780-c5ac-11e8-8b3f-1b3c02d7dcde.gif)
+
+## How to install
+
+Verify if you have [node](http://nodejs.org/) and [npm](https://www.npmjs.org/) installed.
+
+### Command Line
+
+```sh
+$ npm install -g pagebone-cli
+```
+
+## Command Line Usage
+
+*Show the CLI version.*
+
+```sh
+$ pagebone-cli --version
+```
+
+*Show all available commands.*
+
+```sh
+$ pagebone-cli --help
+```
+
+*Create new project using the pagebone Boilerplate.*
+
+```sh
+$ pagebone-cli new <projectName>
+```
+
+<hr>
+
+## Versioning
+
+To keep better organization of releases we follow the [Semantic Versioning 2.0.0](http://semver.org/) guidelines.
+
+## History
+
+See [Releases](https://github.com/jrstylle/pagebone-cli/releases) for detailed changelog.
+
+## License
+
+[MIT License](https://github.com/jrstylle/pagebone-cli/blob/master/LICENSE.md) © [José Rodrigues da Silva Junior](https://joserodrigues.me)
+
+
+
---
+++ bin/index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const download = require('download');
+const cli = require('commander');
+const tree = require('tree-directory');
+const ora = require('ora');
+const packageFile =  require('../package.json');
+
+function help() {
+  return console.log(`
+Usage: ${packageFile.name} [options] or [commands]
+
+A simple CLI app to create new project usign pagebone Boilerplate https://github.com/jrstylle/pagebone#readme
+
+Options:
+
+-v --version        Show the CLI version
+-h --help           Show all available commands and options
+
+Commands:
+
+new <projectName>  Create new project using the pagebone Boilerplate
+  `);
+}
+
+cli.version(`version ${packageFile.version}`, '-v, --version');
+
+cli.command('new <projectName>').action( projectName => {
+
+    const spinner = ora({color: "cyan"}).start('Downloading the project...');
+
+    download('https://github.com/jrstylle/pagebone/archive/master.zip',`${projectName}`, { extract: true, strip: 1, mode: 666, headers: { accept: 'application/zip' } })
+    .then(() => {
+
+      spinner.succeed(`Download complete\n\n${projectName}`);
+      tree(`${projectName}`).then( res => {
+
+        console.log(res);
+      });
+    })
+    .catch( err => {
+
+      spinner.fail('Download failed');
+      console.log(err);
+    });
+});
+
+(!process.argv[2] || process.argv[2] === '-h' || process.argv[2] === '--help')
+  ? help()
+  : cli.parse(process.argv);
+
